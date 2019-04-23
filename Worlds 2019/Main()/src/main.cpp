0787d7677c51c2a86ad4201132bce1a6c8435535
Dean <dareiter02@gmail.com>
2019-04-22 19:32:21 -0700

    gyro PI broke, auton values needed

--- Worlds 2019/Main()/src/main.cpp
+++ Worlds 2019/Main()/src/main.cpp
@@ -1,6 +1,11 @@
 
 #include "Common/main.h"
+void auton() {
+  resetGyro();
+  gyroTurn(90);
+}
 
 int main() {
   Competition.drivercontrol(defaultUserControl);
+  Competition.autonomous(auton);
 }
